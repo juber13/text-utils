a8aff14871ded00d2eec487098422d04b4c02806
juber <juberk1010@gmail.com>
2024-01-17 21:22:52 +0530

    make all function using reducer

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
 const Body = ({text}) => {
+    console.log(text)
     return (
         <div className=''>
             <div className="heading flex flex-col gap-4">
                 <h1 className='text-3xl'>Summery Of Your Text</h1>
-                <p>Nummber of words : {text.split(" ").length}</p>
+                <p>Nummber of words : {text ?  text.split(" ").length : 0}</p>
                 <p>Number of charecters : {text.split("").length}</p>
                 <p>Reading Time:{text.split(" ").length / (120 / 60)}</p>
             </div>
